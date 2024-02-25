96dbd08f1193f436acc0523ee65cb3ffcf5f8d41
markdavies <markdavies550@gmail.com>
2024-02-25 16:26:52 +0000

    thing

--- Camera.gd
+++ Camera.gd
@@ -11,24 +11,27 @@ class_name Camera
 func _process(delta):
 	var position = global_position
 	
-	
 	if Input.is_action_pressed("move_right"):
 		var x  = position.x + (_speed * delta)
 		var min_x = _clamp_x_min.position.x
 		var max_x = _clamp_x_max.position.x
 		position.x = clamp(x, min_x, max_x)
+		
 	elif Input.is_action_pressed("move_left"):
 		var x  = position.x - (_speed * delta)
 		var min_x = _clamp_x_min.position.x
 		var max_x = _clamp_x_max.position.x
 		position.x = clamp(x, min_x, max_x)
+		
 	elif  Input.is_action_pressed("move_down"):
-		var to_zoom = clamp((_speed * delta), _min_zoom, _max_zoom)
-		zoom -= Vector2(to_zoom, to_zoom)
+		var z = zoom.x + (_speed * delta)
+		var to_zoom = clamp(z, _min_zoom, _max_zoom)
+		zoom += Vector2(to_zoom, to_zoom)
 		
 	elif  Input.is_action_pressed("move_up"):
-		var to_zoom = clamp((_speed * delta), _min_zoom, _max_zoom)
-		zoom += Vector2(to_zoom, to_zoom)
+		var z = zoom.x + (_speed * delta)
+		var to_zoom = clamp(z, _min_zoom, _max_zoom)
+		zoom -= Vector2(to_zoom, to_zoom)
 		
 	
 	global_position = position
